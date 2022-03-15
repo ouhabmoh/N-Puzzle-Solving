a638018a992e6f4aa200f3b0304d9820c7fdf353
ohbsimo <mohamed-moh98@live.com>
2022-03-15 06:53:23 +0100

    Add MemoryUsage Stat

--- src/Model/TimeTest.java
+++ src/Model/TimeTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.Instant;
 
 public class TimeTest {
+    static final long MEGABYTE_FACTOR = 1024L * 1024L;
 
     public static long testTime(Runnable exp) {
         Instant debut = Instant.now();
@@ -23,5 +24,30 @@ public class TimeTest {
 
 
         return average / nbTry;
+    }
+
+    public static double getMemoryUsage(){
+        return bytesToMiB(getUsedMemory());
+    }
+
+    public static long getTotalMemory() {
+        return Runtime.getRuntime().totalMemory();
+    }
+
+    private static double bytesToMiB(long bytes) {
+        return ((double) bytes / MEGABYTE_FACTOR);
+    }
+
+    public static long getMaxMemory() {
+        return Runtime.getRuntime().maxMemory();
+    }
+
+    public static long getUsedMemory() {
+        return getTotalMemory() - getFreeMemory();
+    }
+
+
+    public static long getFreeMemory() {
+        return Runtime.getRuntime().freeMemory();
     }
 }
