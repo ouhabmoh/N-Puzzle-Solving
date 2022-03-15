73bafdbd95dbf8e52ece9d767dd57b0a10499ef5
ohbsimo <mohamed-moh98@live.com>
2022-03-15 06:55:46 +0100

    Add MemoryUsage to Results

--- src/Model/Results.java
+++ src/Model/Results.java
@@ -15,23 +15,26 @@ public class Results {
     private final int etatDeveloper;
     private final int etatExplorer;
     private final String algo;
+    private final double memory;
 
-    public Results(Taquin taquinInitial, List<Action> solution, long time, int etatDeveloper, int etatExplorer, String algo) {
+    public Results(Taquin taquinInitial, List<Action> solution, long time, int etatDeveloper, int etatExplorer, String algo, double memory) {
         this.taquinInitial = taquinInitial;
         this.solution = solution;
         this.time = time;
         this.etatDeveloper = etatDeveloper;
         this.etatExplorer = etatExplorer;
         this.algo = algo;
+        this.memory = memory;
     }
 
-    public Results(Recherche recherche, long time) {
+    public Results(Recherche recherche, long time, double memory) {
         this.taquinInitial = recherche.getRoot().getTaquin();
         this.solution = recherche.getSolution();
         this.time = time;
         this.etatDeveloper = recherche.getOuvertSize();
         this.etatExplorer = recherche.getFermerSize();
         this.algo = recherche.toString();
+        this.memory = memory;
     }
 
     public void save() {
@@ -46,6 +49,7 @@ public class Results {
             out.write(etatDeveloper + "\t");
             out.write(etatExplorer + "\t");
             out.write(algo + "\t");
+            out.write(memory+" \t");
             for (Action action : solution)
                 out.write(action.toString() + "\t");
             out.write("\n");
